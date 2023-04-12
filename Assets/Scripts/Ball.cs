559c9a11c9bcadf95f6cd5a9871d354560597d1b
ErdemKaya <kayaerdem936@gmail.com>
2023-04-12 20:23:20 +0300

    Add SetColor method on Ball.cs
    SetColor method added to implement assigment of random color setting

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,11 +5,17 @@ namespace Assets.Scripts
     [RequireComponent(typeof(SpriteRenderer))]
     public class Ball : MonoBehaviour
     {
-        public Color Color { get => _spriteRenderer.color; }
+        public Color Color 
+        { 
+            get => _spriteRenderer.color;
+            set => _spriteRenderer.color = value;
+        }
         protected SpriteRenderer _spriteRenderer;
 
         //TODO: Implement 2 types of balls which has red and blue colors.
         //OPTIONAL: Think outside the box.
+
+        public Color[] colors = {Color.red, Color.blue,};
 
         protected virtual void Awake()
         {
@@ -19,6 +25,12 @@ namespace Assets.Scripts
         public void SetName(string name)
         {
             this.name = name;
+        }
+
+        public void SetColor()
+        {
+            int randomIndexOfColors = Random.Range(0, colors.Length);
+            this.Color = colors[randomIndexOfColors];
         }
     }
 }
