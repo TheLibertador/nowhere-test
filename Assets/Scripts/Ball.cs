5faf04c8d4f1ddb78320d9b287873176b515d51c
Tarik Bir <7569835+tarikbir@users.noreply.github.com>
2023-04-12 16:49:55 +0300

    Update Ball.cs

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,6 +4,8 @@ namespace Assets.Scripts
 {
     public class Ball : MonoBehaviour
     {
+        //TODO: Implement 2 different types of balls with different sounds.
+        
         public void SetName(string name)
         {
             this.name = name;
