f410bfae2f2739ba745321a745f0b40a3cd63bd1
ErdemKaya <kayaerdem936@gmail.com>
2023-04-13 16:17:50 +0300

    Update SpawnManager.cs to spawn and remove ball
    Now we can add random colored balls to the scene with left mouse click. Also we can remove all the red balls with right mouse click.

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Assets.Scripts
@@ -7,7 +8,8 @@ namespace Assets.Scripts
     {
         [SerializeField] private GameObject _ballPrefab;
 
-        private List<Ball> _balls;
+        private List<Ball> _balls = new List<Ball>();
+        //private List<Ball> _ballsToRemove = new List<Ball>();
 
         private void Update()
         {
@@ -18,33 +20,64 @@ namespace Assets.Scripts
             else if (Input.GetMouseButtonDown(1))
             {
                 //TODO: This should remove all red balls, right?
-                foreach (var ball in _balls)
+                
+                //We can't use only one foreach because in that situation we are trying to remove an item from the list 
+                //while we are iterating over it. This gives an error. Instead of we can create another list and use two
+                //foreach loops or we can use only one for loop. I choose using one for loop because it is more
+                //efficient.
+
+                
+                for (int i = _balls.Count - 1; i >= 0; i--)
                 {
-                    if (ball.Color == Color.red)
+                    if (_balls[i].Color == Color.red)
                     {
-                        RemoveBall(ball);
-                        break;
+                        RemoveBall(_balls[i]);
                     }
                 }
+                
+                /*
+                other solution:
+                 
+                foreach (var ball in _balls)
+                { 
+                    if (ball.Color == Color.red)
+                    {
+                        _ballsToRemove.Add(ball);
+                        //break;
+                    }
+                }
+
+                foreach (var ball in _ballsToRemove)
+                {
+                    RemoveBall(ball);
+                }*/
+                
             }
         }
 
         public void RemoveBall(Ball ball)
         {
             //TODO: Implement a way to remove a ball from the scene and the list.
+            _balls.Remove(ball);
+            Destroy(ball.gameObject);
         }
 
         private void SpawnBallAtMouseAndGiveName(Vector3 mousePosition, string name)
         {
             //TODO: Spawn a random color of ball at the position of the mouse click and play spawn sound.
             //OPTIONAL: Use events for playing sounds.
+            mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            mousePosition.z = 0f;
+            InstantiateBall(mousePosition, name);
         }
 
-        private Ball InstantiateBall(Vector3 position)
-        {
-            GameObject ballGameObject = Instantiate(_ballPrefab, position, Quaternion.identity);
-            Ball ball = ballGameObject.GetComponent<Ball>(); //TODO: What's a better way to do this?
+        private Ball InstantiateBall(Vector3 position, string name)
+        { 
+            //TODO: What's a better way to do this?
+            //There is no need for gameObject declaration. We just need to use the ball component of the objects.
+            Ball ball = Instantiate(_ballPrefab, position, Quaternion.identity).GetComponent<Ball>();
             ball.SetName(name);
+            ball.SetColor();
             _balls.Add(ball);
             return ball;
         }
